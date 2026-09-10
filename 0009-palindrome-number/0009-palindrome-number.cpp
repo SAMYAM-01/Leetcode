fb7d8b03f6f063f3ed5c9250f0d95e766b84dfc5
SAMYAM-01 <samyamsunil23@gmail.com>
2026-09-10 20:53:15 +0530

    Time: 2 ms (50.02%), Space: 8.4 MB (92.02%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+      
+
+      if(x<0)
+      {
+        return false;
+      }
+
+        long long int rev=0;
+        int original=x;
+
+
+     
+      while(x!=0)
+      {
+        int digit=x%10;
+   rev=rev*10+digit;
+   x=x/10;
+      }
+return rev==original;
+
+    }
+};
